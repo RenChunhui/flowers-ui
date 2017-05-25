796738561846650acdbd77fd36879217171d2954
Chunhui Ren <chunhuiren@Chunhuis-MacBook-Pro.local>
2017-05-25 16:39:18 +0800

    commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+/**
+ * @description: App
+ * @author: renchunhui2008@gmail.com
+ * 
+ * Copyright Ren Chunhui. All Rights Reserved.
+ * 
+ * This program is free software. You can redistribute and/or modify it
+ * in accordance with the terms of the accompanying license agreement.
+ */
+
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return(
+            <div>
+                Hello,World
+            </div>
+        )
+    }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,18 @@
+/**
+ * @description: 入口文件
+ * @author: Ren Chunhui
+ * 
+ * Copyright (c) 2017 Ren Chunhui 
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+import './styles/style.scss';
+
+ReactDOM.render((
+    <App />
+),document.getElementById('app'));
