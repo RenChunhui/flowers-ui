1513a5ecdf3fa2e649754a938e575bf0ba17aa1e
春晖 <chunhui@chunhuideMacBook-Pro.local>
2017-05-26 16:21:21 +0800

    添加eslint规则

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": ["eslint:recommended", "plugin:react/recommended"],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true,
+            "jsx": true
+        },
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "react/jsx-uses-react": "error",
+        "react/jsx-uses-vars": "error",
+    }
+};
---
+++ src/views/ButtonView.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class ButtonView extends React.Component {
+
+    render() {
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default ButtonView;
